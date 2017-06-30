37f8a817ee4a24ea35ba1ca3c622d4b2594b36e9
sametaylak <sametaylak29@gmail.com>
2017-07-01 02:02:01 +0300

    Added random obstacle

--- Assets/Scripts/SimpleObject.cs
+++ Assets/Scripts/SimpleObject.cs
@@ -3,7 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleObject : MonoBehaviour {
+	public GameObject obstacle;
+
+	private GameObject spawner;
+
+	void Start(){
+		spawner = this.gameObject.transform.GetChild (1).gameObject;
+	}
+
 	void OnTriggerEnter(Collider col) {
 		FindObjectOfType<SimpleManager> ().SendMessage ("CreateTile");
+		Instantiate (obstacle, RandomPointInBox (spawner.transform.position), Quaternion.identity);
+	}
+
+	Vector3 RandomPointInBox(Vector3 center) {
+		return center + new Vector3(
+			Random.Range(-1.5f, 1.5f),
+			0.5f,
+			20f
+		);
 	}
 }
